9c6ca904a077d6401fef98d38b5deb870fac1613
dexslab <dex35803@gmail.com>
2020-06-30 22:50:47 -0400

    File structure changes to allow installer compile, fixes for gitignore

--- Config.h
+++ Config.h
@@ -22,8 +22,8 @@
 
 // Choose the motor shield that you want to use.
 
-#define CONFIG_WSM_FIREBOX
-//#define CONFIG_ARDUINO_MOTOR_SHIELD
+//#define CONFIG_WSM_FIREBOX
+#define CONFIG_ARDUINO_MOTOR_SHIELD
 //#define CONFIG_POLOLU_MOTOR_SHIELD
 
 #endif
