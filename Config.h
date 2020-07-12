0d0f591d2970b4f29f7c07d7c2c14c10fd2bc47b
Harald Barth <haba@kth.se>
2020-07-12 16:38:06 +0200

    revert Config.h to standard

--- Config.h
+++ Config.h
@@ -8,25 +8,25 @@
 
 const byte UNUSED_PIN = 255;
 
-const byte MAIN_POWER_PIN = 4;
-const byte MAIN_SIGNAL_PIN = 7;
+const byte MAIN_POWER_PIN = 3;
+const byte MAIN_SIGNAL_PIN = 12;
 const byte MAIN_SIGNAL_PIN_ALT = UNUSED_PIN;  // for hardware that flipflops signal pins 
 const byte MAIN_SENSE_PIN = A0;   
 const byte MAIN_BRAKE_PIN = 9;
-const byte MAIN_FAULT_PIN = 12;
+const byte MAIN_FAULT_PIN = UNUSED_PIN;
 
 const int   MAIN_MAX_MILLIAMPS=2000;
-const float MAIN_SENSE_FACTOR=18; //  analgRead(MAIN_SENSE_PIN) * MAIN_SENSE_FACTOR = milliamps 
+const float MAIN_SENSE_FACTOR=2.99; //  analgRead(MAIN_SENSE_PIN) * MAIN_SENSE_FACTOR = milliamps 
 
-const byte PROG_POWER_PIN = 2;
-const byte PROG_SIGNAL_PIN = 8;
+const byte PROG_POWER_PIN = 11;
+const byte PROG_SIGNAL_PIN = 13;
 const byte PROG_SIGNAL_PIN_ALT = UNUSED_PIN;  // for hardware that flipflops signal pins 
 const byte PROG_SENSE_PIN = A1;
-const byte PROG_BRAKE_PIN = 10;
+const byte PROG_BRAKE_PIN = 8;
 const byte PROG_FAULT_PIN = UNUSED_PIN;
 
 const int   PROG_MAX_MILLIAMPS=250;
-const float PROG_SENSE_FACTOR=18; //  analgRead(PROG_SENSE_PIN) * PROG_SENSE_FACTOR = milliamps 
+const float PROG_SENSE_FACTOR=2.99; //  analgRead(PROG_SENSE_PIN) * PROG_SENSE_FACTOR = milliamps 
 
 // Allocations with memory implications..!
 // Base system takes approx 900 bytes + 8 per loco. Turnouts, Sensors etc are dynamically created
