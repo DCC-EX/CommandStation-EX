9e86d5e93c48536a76922152791cf91b933c586a
David Cutting <davidcutting42@gmail.com>
2020-07-14 19:04:55 -0600

    Update interrupt handling for better waveforms

--- CommandStation.cpp
+++ CommandStation.cpp
@@ -38,8 +38,10 @@ DCCService* progTrack = DCCService::Create_Pololu_MC33926Shield_Prog();
 #endif
 
 void waveform_IrqHandler() {
-  mainTrack->interruptHandler();
-  progTrack->interruptHandler();
+  bool mainInterrupt = mainTrack->interrupt1();
+  bool progInterrupt = progTrack->interrupt1();
+  if(mainInterrupt) mainTrack->interrupt2();
+  if(progInterrupt) progTrack->interrupt2();
 }
 
 #if defined(ARDUINO_ARCH_SAMD)
