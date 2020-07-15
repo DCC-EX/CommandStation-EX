8d4d9bd28e545ccb5d09adf99e986d536a0d62e9
Asbelos <asbelos@btinternet.com>
2020-07-15 20:26:19 +0100

    Update objdump.bat
    Even more sorted info

--- objdump.bat
+++ objdump.bat
@@ -1,6 +1,12 @@
 ECHO ON
 FOR /F "delims=" %%i IN ('dir %TMP%\arduino_build_* /b /ad-h /t:c /od') DO SET a=%%i
-echo Most recent subfolder: %a%
-avr-objdump -x -C %TMP%\%a%\CVReader.ino.elf | find ".text" | sort /+25 /R >%TMP%\OBJDUMP_%a%.txt
+echo Most recent subfolder: %a% >%TMP%\OBJDUMP_%a%.txt
+avr-objdump --private=mem-usage  %TMP%\%a%\CVReader.ino.elf  >>%TMP%\OBJDUMP_%a%.txt
+ECHO ++++++++++++++++++++++++++++++++++ >>%TMP%\OBJDUMP_%a%.txt
+avr-objdump -x -C %TMP%\%a%\CVReader.ino.elf | find ".text" | sort /+25 /R >>%TMP%\OBJDUMP_%a%.txt
+ECHO ++++++++++++++++++++++++++++++++++ >>%TMP%\OBJDUMP_%a%.txt
+avr-objdump -x -C %TMP%\%a%\CVReader.ino.elf | find ".data" | sort /+25 /R >>%TMP%\OBJDUMP_%a%.txt
+ECHO ++++++++++++++++++++++++++++++++++ >>%TMP%\OBJDUMP_%a%.txt
+avr-objdump -x -C %TMP%\%a%\CVReader.ino.elf | find ".bss" | sort /+25 /R >>%TMP%\OBJDUMP_%a%.txt
 notepad %TMP%\OBJDUMP_%a%.txt
 
