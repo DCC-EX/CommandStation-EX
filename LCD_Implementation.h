49d929a522b0c4fff8dab9f4cc8919ccb3f0ea85
Asbelos <asbelos@btinternet.com>
2020-10-10 16:40:06 +0100

    License/credits only

--- LCD_Implementation.h
+++ LCD_Implementation.h
@@ -1,8 +1,24 @@
+/*
+ *  © 2020, Chris Harlow. All rights reserved.
+ *  
+ *  This file is part of CommandStation-EX
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 ////////////////////////////////////////////////////////////////////////////////////
-//  © 2020, Chris Harlow. All rights reserved.
-//
-// This implementation is designed to be #included ONLY ONCE in the .ino after config.h
+// This implementation is designed to be #included ONLY ONCE in the .ino 
 //
 // It will create a driver implemntation and a shim class implementation.
 // This means that other classes can reference the shim without knowing
--- LCD_OLED.h
+++ LCD_OLED.h
@@ -1,3 +1,22 @@
+/*
+ *  © 2020, Chris Harlow. All rights reserved.
+ *  
+ *  This file is part of CommandStation-EX
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // OLED Implementation of LCDDisplay class 
 // Note: this file is optionally included by LCD_Implenentation.h
 // It is NOT a .cpp file to prevent it being compiled and demanding libraraies even when not needed.
