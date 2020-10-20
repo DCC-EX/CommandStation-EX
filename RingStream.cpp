8c8a4846716762ec91db77cfa5737cb46d0b0cfd
Asbelos <asbelos@btinternet.com>
2020-10-20 12:48:32 +0100

    License details correction

--- RingStream.cpp
+++ RingStream.cpp
@@ -1,26 +1,21 @@
 /*
-
- (c) 2015 Ingo Fischer
- buffer serial device
- based on Arduino SoftwareSerial
-
- Constructor warning messages fixed by Chris Harlow.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-*/
+ *  © 2020, Chris Harlow. All rights reserved.
+ *  
+ *  This file is part of DCC-EX CommandStation-EX
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "RingStream.h"
 #include "DIAG.h"
