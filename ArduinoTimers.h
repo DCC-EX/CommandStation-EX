eececa322a8c31adb871ce29296521a393b443d1
dexslab <dex35803@gmail.com>
2020-11-20 08:34:36 -0500

    Start adding back unowifi stuffz

--- ArduinoTimers.h
+++ ArduinoTimers.h
@@ -3,16 +3,18 @@
 // The files included below come from the same source.
 // This library had been included with the DCC code to avoid issues with
 // library management for inexperienced users. "It just works (TM)"
- 
+
 #ifndef ArduinoTimers_h
 #define ArduinoTimers_h
 
 #if defined(ARDUINO_AVR_MEGA) || defined(ARDUINO_AVR_MEGA2560)
-    #include "ATMEGA2560/Timer.h"
+#include "ATMEGA2560/Timer.h"
 #elif defined(ARDUINO_AVR_UNO)
-    #include "ATMEGA328/Timer.h"
+#include "ATMEGA328/Timer.h"
+#elif defined(ARDUINO_ARCH_MEGAAVR)
+#include "ATMEGA4809/Timer.h"
 #else
-    #error "Cannot compile - ArduinoTimers library does not support your board, or you are missing compatible build flags."
+#error "Cannot compile - ArduinoTimers library does not support your board, or you are missing compatible build flags."
 #endif
 
 #endif
