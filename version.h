2ce4c8066e49c6bbbc5073db480c860644d00736
Asbelos <asbelos@btinternet.com>
2021-01-21 11:10:52 +0000

    Update version.h

--- version.h
+++ version.h
@@ -4,7 +4,10 @@
 #include "StringFormatter.h"
 
 // const char VERSION[] PROGMEM ="0.2.0";
-#define VERSION "3.0.2"
-
+#define VERSION "3.0.3"
+// 3.0.3 Includes:
+//  <W addr> command to write loco address and clear consist 
+//  <R> command will allow for consist address
+//  Startup commands implemented
 
 #endif
