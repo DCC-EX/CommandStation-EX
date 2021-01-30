13dec796c100d8ee68fb7af534f75230d0ed812c
Harald Barth <haba@kth.se>
2021-01-30 15:36:28 +0100

    version++

--- version.h
+++ version.h
@@ -4,7 +4,9 @@
 #include "StringFormatter.h"
 
 // const char VERSION[] PROGMEM ="0.2.0";
-#define VERSION "3.0.3"
+#define VERSION "3.0.4"
+// 3.0.4 Includes:
+// Wifi startup bugfixes
 // 3.0.3 Includes:
 //  <W addr> command to write loco address and clear consist 
 //  <R> command will allow for consist address
