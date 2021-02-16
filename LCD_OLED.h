4647568b8d27e0d8e564c08baeacacb51af9be0e
Neil McKechnie <neilmck999@gmail.com>
2021-02-16 23:38:33 +0000

    Fast SSD1306 OLED driver
    Incorporate code from SSD1306Ascii library to speed up OLED screen updates, reduce memory requirements and eliminate some library dependences.

--- LCD_OLED.h
+++ LCD_OLED.h
@@ -1,5 +1,5 @@
 /*
- *  © 2020, Chris Harlow. All rights reserved.
+ *  © 2020, Chris Harlow, 2021, Neil McKechnie. All rights reserved.
  *  
  *  This file is part of CommandStation-EX
  *
@@ -18,40 +18,46 @@
  */
 
 // OLED Implementation of LCDDisplay class 
-// Note: this file is optionally included by LCD_Implenentation.h
-// It is NOT a .cpp file to prevent it being compiled and demanding libraraies even when not needed.
+// Note: this file is optionally included by LCD_Implementation.h
+// It is NOT a .cpp file to prevent it being compiled and demanding libraries even when not needed.
   
-#include <Adafruit_SSD1306.h>
-Adafruit_SSD1306 LCDDriver(OLED_DRIVER);
+#include "SSD1306/SSD1306Ascii.h"
+#include "SSD1306/SSD1306AsciiWire.h"
+SSD1306AsciiWire LCDDriver;
   
 // DEVICE SPECIFIC LCDDisplay Implementation for OLED
 
-LCDDisplay::LCDDisplay() {
-    if(LCDDriver.begin(SSD1306_SWITCHCAPVCC, 0x3C) || LCDDriver.begin(SSD1306_SWITCHCAPVCC, 0x3D)) {
-      DIAG(F("\nOLED display found")); 
-      delay(2000); // painful Adafruit splash pants! 
-      lcdDisplay=this;
-      LCDDriver.setTextSize(1);             // Normal 1:1 pixel scale
-      LCDDriver.setTextColor(SSD1306_WHITE);            // Draw white text
-      interfake(OLED_DRIVER,0);
-      clear();
-      return;  
+  LCDDisplay::LCDDisplay() {
+    // Scan for device on 0x3c and 0x3d.
+    for (byte address=0x3c; address<=0x3d; address++) {
+      Wire.beginTransmission(address);
+      byte error = Wire.endTransmission();
+      if (!error) {
+        // Device found
+        DIAG(F("\nOLED display found at 0x%x"), address); 
+        interfake(OLED_DRIVER,0);
+        LCDDriver.begin(lcdRows==32 ? &Adafruit128x32 : &Adafruit128x64, address);
+        lcdDisplay=this;
+        LCDDriver.setFont(System5x7);             // Normal 1:1 pixel scale
+        clear();
+        return;  
       }
-    DIAG(F("\nOLED display not found\n"));
     }
+    DIAG(F("\nOLED display not found\n"));
+  }
 
   void LCDDisplay::interfake(int p1, int p2, int p3) {(void)p1; lcdRows=p2/8; (void)p3;}   
 
-  void LCDDisplay::clearNative() {LCDDriver.clearDisplay();}
+  void LCDDisplay::clearNative() {LCDDriver.clear();}
 
   void LCDDisplay::setRowNative(byte row) {
     // Positions text write to start of row 1..n and clears previous text 
-    int y=8*row;
-    LCDDriver.fillRect(0, y, LCDDriver.width(),   8, SSD1306_BLACK);
+    int y=row;
     LCDDriver.setCursor(0, y); 
-    }
+    LCDDriver.clearToEOL();
+  }
   
-  void LCDDisplay::writeNative(char * b){ LCDDriver.print(b); }    
+  void LCDDisplay::writeNative(char * b){ LCDDriver.write(b); }    
   
-  void LCDDisplay::displayNative() { LCDDriver.display(); }  
+  void LCDDisplay::displayNative() {  }  
   
