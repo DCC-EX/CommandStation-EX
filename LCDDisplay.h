5be7209bf4d2fc498506faeff4bb86bcdc97e6fc
Neil McKechnie <neilmck999@gmail.com>
2021-02-25 17:12:02 +0000

    Scrolling updates
    New scrolling capability by defining SCROLLMODE in Config.h to 0 (original), 1 (by page) or 2 (by line). If not defined, defaults to 0.
    Reformat.

--- LCDDisplay.h
+++ LCDDisplay.h
@@ -1,6 +1,6 @@
 /*
  *  © 2021, Chris Harlow, Neil McKechnie. All rights reserved.
- *  
+ *
  *  This file is part of CommandStation-EX
  *
  *  This is free software: you can redistribute it and/or modify
@@ -23,50 +23,49 @@
 // This class is created in LCDisplay_Implementation.h
 
 class LCDDisplay : public Print {
+ public:
+  static const int MAX_LCD_ROWS = 8;
+  static const int MAX_LCD_COLS = 16;
+  static const long LCD_SCROLL_TIME = 3000;  // 3 seconds
 
-  public:
-    static const int MAX_LCD_ROWS=8;
-    static const int MAX_LCD_COLS=16;
-    static const long LCD_SCROLL_TIME=3000; // 3 seconds  
-    
-    static LCDDisplay* lcdDisplay; 
-    LCDDisplay();
-    void interfake(int p1, int p2, int p3);    
+  static LCDDisplay* lcdDisplay;
+  LCDDisplay();
+  void interfake(int p1, int p2, int p3);
 
-    // Internally handled functions
-    static void loop();
-    LCDDisplay*  loop2(bool force);
-    void setRow(byte line); 
-    void clear();
-   
-    virtual size_t write(uint8_t b);
-    using Print::write;
-    
-  private:
-    int nextFilledRow();
-  
-   // Relay functions to the live driver
-    void clearNative();
-    void displayNative();
-    void setRowNative(byte line); 
-    void writeNative(char b);
-  
-   unsigned long lastScrollTime=0;
-   int8_t hotRow=0;
-   int8_t hotCol=0; 
-   int8_t topRow=0;
-   uint8_t lcdRows;
-   uint8_t lcdCols;
-   int8_t slot=0;
-   int8_t rowFirst=-1;
-   int8_t rowNext=0;
-   int8_t charIndex = 0;
-   char buffer[MAX_LCD_COLS+1];
-   char *bufferPointer = 0;
-   bool done = false;
+  // Internally handled functions
+  static void loop();
+  LCDDisplay* loop2(bool force);
+  void setRow(byte line);
+  void clear();
 
-   void renderRow(byte row);
-   char rowBuffer[MAX_LCD_ROWS][MAX_LCD_COLS+1];     
+  virtual size_t write(uint8_t b);
+  using Print::write;
+
+ private:
+  void moveToNextRow();
+
+  // Relay functions to the live driver
+  void clearNative();
+  void displayNative();
+  void setRowNative(byte line);
+  void writeNative(char b);
+
+  unsigned long lastScrollTime = 0;
+  int8_t hotRow = 0;
+  int8_t hotCol = 0;
+  int8_t topRow = 0;
+  uint8_t lcdRows;
+  uint8_t lcdCols;
+  int8_t slot = 0;
+  int8_t rowFirst = -1;
+  int8_t rowNext = 0;
+  int8_t charIndex = 0;
+  char buffer[MAX_LCD_COLS + 1];
+  char* bufferPointer = 0;
+  bool done = false;
+
+  void renderRow(byte row);
+  char rowBuffer[MAX_LCD_ROWS][MAX_LCD_COLS + 1];
 };
 
 #endif
