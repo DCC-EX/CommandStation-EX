3c30ef3c9d368bc42d2d47faf2716612247c2fbc
Harald Barth <haba@kth.se>
2021-04-06 22:10:33 +0200

    3.0.11

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "3.0.10"
+#define VERSION "3.0.11"
 // 3.0.10 Teensy Support
 // 3.0.9 rearranges serial newlines for the benefit of JMRI.
 // 3.0.8 Includes <* *> wraps around DIAGs for the benefit of JMRI.
