5bc5120590b19d1d014b886e52ca62a1663cb420
FrightRisk <37218136+FrightRisk@users.noreply.github.com>
2021-04-20 18:28:33 -0400

    Add more old firmware checks and set oldCmd earlier

--- version.h
+++ version.h
@@ -4,6 +4,8 @@
 #include "StringFormatter.h"
 
 #define VERSION "3.0.11"
+// 3.0.12 fix esp8266 test for new firmware
+// 3.0.11 ?
 // 3.0.10 Teensy Support
 // 3.0.9 rearranges serial newlines for the benefit of JMRI.
 // 3.0.8 Includes <* *> wraps around DIAGs for the benefit of JMRI.
