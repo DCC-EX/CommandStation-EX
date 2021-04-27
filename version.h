45eb7c80b658687cbbf2796fa876535515fb59c3
Fred <fndecker@gmail.com>
2021-04-27 10:37:54 -0400

    Frightrisk hostname fix (#144)
    * Fix esp8266 hostname in STA mode by checking for new version of the AT cmd instead of old cmd since some firmware still allows old commands
    
    * Add more old firmware checks and set oldCmd earlier
    
    * increment version number

--- version.h
+++ version.h
@@ -3,7 +3,9 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "3.0.11"
+#define VERSION "3.0.12"
+// 3.0.12 fix esp8266 test for new firmware
+// 3.0.11 ?
 // 3.0.10 Teensy Support
 // 3.0.9 rearranges serial newlines for the benefit of JMRI.
 // 3.0.8 Includes <* *> wraps around DIAGs for the benefit of JMRI.
