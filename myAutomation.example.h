329df3a3ee627dba89915b065a51767c51586f30
Asbelos <asbelos@btinternet.com>
2021-08-12 20:35:56 +0100

    correct example sensors

--- myAutomation.example.h
+++ myAutomation.example.h
@@ -33,8 +33,8 @@ EXRAIL   // myAutomation must start with the EXRAIL instruction
   DONE     // This just ends the startup thread, leaving 2 others running.
 
 /* SEQUENCE(1) is a simple shuttle between 2 sensors      
- *  S10 and S11 are sensors pre-defined with the <S> command
- *  S10                    S11                   
+ *  S20 and S21 are sensors on arduino pins 20 and 21 
+ *  S20                    S21                   
  *  === START->================
  */
    SEQUENCE(1) 
@@ -43,41 +43,41 @@ EXRAIL   // myAutomation must start with the EXRAIL instruction
      DELAY(1000)    // wait 1 second
      FOFF(3)      // Horn off
      FWD(80)      // Move forward at speed 80
-     AT(11)       // until we hit sensor id 11
+     AT(21)       // until we hit sensor id 21
      STOP         // then stop
      DELAY(5000)    // Wait 5 seconds
      FON(2)       // ring bell
      REV(60)      // reverse at speed 60
-     AT(10)       // until we get to S10
+     AT(20)       // until we get to S20
      STOP         // then stop
      FOFF(2)      // Bell off 
      FOLLOW(1)    // and follow sequence 1 again
    
 /* SEQUENCE(2) is an automation example for a single loco Y shaped journey
- *  S1,S2,S3 are sensors, T4 is a turnout
+ *  S31,S32,S33 are sensors, T4 is a turnout
  *  
- *  S3                      T4                            S1
+ *  S33                      T4                            S31
  *  ===-START->=============================================
  *                          //
- *  S2                     //
+ *  S32                    //
  *  ======================//
  *  
- *  Train runs from START to S1, back to S2, again to S1, Back to start.
+ *  Train runs from START to S31, back to S32, again to S31, Back to start.
  */
   SEQUENCE(2)
    FWD(60)     // go forward at DCC speed 60 
-   AT(1) STOP  // when we get to sensor 1 
+   AT(31) STOP  // when we get to sensor 31 
    DELAY(10000)  // wait 10 seconds 
-   THROW(4)    // throw turnout for route to S2
+   THROW(4)    // throw turnout for route to S32
    REV(45)     // go backwards at speed 45
-   AT(2) STOP  // until we arrive at sensor 2
+   AT(32) STOP  // until we arrive at sensor 32
    DELAY(5000)   // wait 5 seconds
    FWD(50)     // go forwards at speed 50
-   AT(1) STOP  // and stop at sensor 1
+   AT(31) STOP  // and stop at sensor 31
    DELAY(5000)   // wait 5 seconds 
    CLOSE(4)    // set turnout closed
    REV(50)     // reverse back to S3
-   AT(3) STOP
+   AT(33) STOP
    DELAY(20000)  // wait 20 seconds 
    FOLLOW(2)   // follow sequence 2... ie repeat the process
    
