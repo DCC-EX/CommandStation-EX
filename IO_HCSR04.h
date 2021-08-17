e9629529f9c6c4ba0a01d765b13b69e7569a24b8
Neil McKechnie <neilmck999@gmail.com>
2021-08-17 22:05:02 +0100

    Update IO_HCSR04.h
    Allow thresholds for ON and OFF to be separately configured at creation.

--- IO_HCSR04.h
+++ IO_HCSR04.h
@@ -55,30 +55,32 @@ private:
   // pins must be arduino GPIO pins, not extender pins or HAL pins.
   int _transmitPin = -1;
   int _receivePin = -1;
-  // Threshold for setting active state in cm.
-  uint16_t _threshold;  // cm
+  // Thresholds for setting active state in cm.
+  uint8_t _onThreshold;  // cm
+  uint8_t _offThreshold; // cm
   // Active=1/inactive=0 state 
   uint8_t _value = 0;
   // Time of last loop execution
   unsigned long _lastExecutionTime;
-  // Hysteresis for deactivating state, in cm
-  const uint16_t hysteresis = 3; // cm
+  // Factor for calculating the distance (cm) from echo time (ms).
+  //  Based on a speed of sound of 345 metres/second.
   const uint16_t factor = 58; // ms/cm
 
 public:
   // Constructor perfroms static initialisation of the device object
-  HCSR04 (VPIN vpin, int transmitPin, int receivePin, uint16_t threshold) {
+  HCSR04 (VPIN vpin, int transmitPin, int receivePin, uint16_t onThreshold, uint16_t offThreshold) {
     _firstVpin = vpin;
     _nPins = 1;
     _transmitPin = transmitPin;
     _receivePin = receivePin;
-    _threshold = threshold;
+    _onThreshold = onThreshold;
+    _offThreshold = offThreshold;
     addDevice(this);
   }
 
   // Static create function provides alternative way to create object
-  static void create(VPIN vpin, int transmitPin, int receivePin, uint16_t threshold) {
-    new HCSR04(vpin, transmitPin, receivePin, threshold);
+  static void create(VPIN vpin, int transmitPin, int receivePin, uint16_t onThreshold, uint16_t offThreshold) {
+    new HCSR04(vpin, transmitPin, receivePin, onThreshold, offThreshold);
   }
 
 protected:
@@ -88,7 +90,8 @@ protected:
     pinMode(_receivePin, INPUT);
     ArduinoPins::fastWriteDigital(_transmitPin, 0);
     _lastExecutionTime = micros();
-    DIAG(F("HCSR04 configured on VPIN:%d TXpin:%d RXpin:%d threshold:%dcm"), _firstVpin, _transmitPin, _receivePin, _threshold);
+    DIAG(F("HCSR04 configured on VPIN:%d TXpin:%d RXpin:%d On:%dcm Off:%dcm"),
+      _firstVpin, _transmitPin, _receivePin, _onThreshold, _offThreshold);
   }
 
   // _read function - just return _value (calculated in _loop).
@@ -102,7 +105,7 @@ protected:
     if (currentMicros - _lastExecutionTime > 50000) {
       _lastExecutionTime = currentMicros;
 
-      read_HCSR04device();
+      _value = read_HCSR04device();
     }
   }
 
@@ -111,18 +114,19 @@ private:
   //  the pulse observed on the receive pin.  In order to be kind to the rest of the CS
   //  software, no interrupts are used and interrupts are not disabled.  The pulse duration
   //  is measured in a loop, using the micros() function.  Therefore, interrupts from other
-  //  sources may affect the result.  To mitigate this, the time between successive loops is
-  //  tested and if it is excessive the current measurement is aborted (previous value is
-  //  retained).  Also, hysteresis is applied on reset: the output is set to 1 when the 
-  //  measured distance is less than the threshold, and is set to 0 if the measured distance is
-  //  greater than the threshold plus 3cm.
+  //  sources may affect the result.  However, interrupts response code in CS typically takes
+  //  much less than the 58us frequency for the DCC interrupt, and 58us corresponds to only 1cm
+  //  in the HC-SR04.
+  //  To reduce chatter on the output, hysteresis is applied on reset: the output is set to 1 when the 
+  //  measured distance is less than the onThreshold, and is set to 0 if the measured distance is
+  //  greater than the offThreshold.
   //
-  void read_HCSR04device() {
+  uint8_t read_HCSR04device() {
     // uint16 enough to time up to 65ms
-    uint16_t startTime, waitTime, currentTime, lastTime, maxTime;  
+    uint16_t startTime, waitTime, currentTime, maxTime;  
 
     // If receive pin is still set on from previous call, abort the read.
-    if (ArduinoPins::fastReadDigital(_receivePin)) return;
+    if (ArduinoPins::fastReadDigital(_receivePin)) return _value;
 
     // Send 10us pulse to trigger transmitter
     ArduinoPins::fastWriteDigital(_transmitPin, 1);
@@ -131,45 +135,37 @@ private:
 
     // Wait for receive pin to be set
     startTime = currentTime = micros();
-    maxTime = factor * _threshold * 2;
+    maxTime = factor * _offThreshold * 2;
     while (!ArduinoPins::fastReadDigital(_receivePin)) {
-      lastTime = currentTime;
+      // lastTime = currentTime;
       currentTime = micros();
       waitTime = currentTime - startTime;
       if (waitTime > maxTime) {
         // Timeout waiting for pulse start, abort the read
-        return;
+        return _value;
       }
     }
-    // Check if loop was unreasonably delayed by interrupts.
-    // If so, abort the read as the measurement might
-    // be reduced by the delay.
-    if (currentTime - lastTime >= factor) return;
 
     // Wait for receive pin to reset, and measure length of pulse
     startTime = currentTime = micros();
-    maxTime = factor * (_threshold + hysteresis);
+    maxTime = factor * _offThreshold;
     while (ArduinoPins::fastReadDigital(_receivePin)) {
-      lastTime = currentTime;
       currentTime = micros();
       waitTime = currentTime - startTime;
       // If pulse is too long then set return value to zero,
       //  and finish without waiting for end of pulse.
       if (waitTime > maxTime) {
-        // Check if loop was unreasonably delayed by interrupts.
-        // If so, abort calculation as the measurement might
-        // have been increased by the delay.
-        if (currentTime - lastTime >= factor) return;
         // Pulse length longer than maxTime, reset value.
-        _value = 0;
-        return;
+        return 0;
       }
     } 
     // Check if pulse length is below threshold, if so set value.
     //DIAG(F("HCSR04: Pulse Len=%l Distance=%d"), waitTime, distance);
     uint16_t distance = waitTime / factor; // in centimetres
-    if (distance < _threshold) 
-      _value = 1;
+    if (distance < _onThreshold) 
+      return 1;
+
+    return _value;
   }
 
 };
