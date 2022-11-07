d086871e2dee87d18c5aedf88e68ba0c22abdbcb
Kcsmith0708 <kcsmith0708@wowway.com>
2022-11-07 16:09:06 -0500

    Version.h 4.1.3 and 4.2.5
    Reconcile 4.0 thru 4.1.3

--- version.h
+++ version.h
@@ -26,31 +26,62 @@
 //       TrackManager DCC & DC up to 8 Districts Architecture 
 //       Automatic ALIAS(name) 
 //       Command Parser now accepts Underscore in Alias Names
+// 4.1.3 Bugfix: Ethernet init order
+// 4.1.2 Bugfix: Ethernet shield W5100 does not report HW or link level
 // 4.1.1 Bugfix: preserve turnout format
-//       Bugfix: parse multiple commands in one buffer string correct
-//       Bugfix: </> command signal status in Exrail
+//       Bugfix: parse multiple commands in one buffer string correctly (ex: <s><Q>)
+//       Bugfix: </> command signal status of EX-RAIL tasks or threads
+//       Bugfix: EX-RAIL read long loco addr
+//       Bugfix: Add space character after version string 4.1.1 for JMRI parsing.
+//       Improved display and loop time for signals make service start to be outside the DONT_TOUCH_WIFI_CONF area
+//       Improve WiFi startup by making service start to be outside the DONT_TOUCH_WIFI_CONF area
 // 4.1.0 ...
-//
-// 4.0.2 EXRAIL additions:
-//       ACK defaults set to 50mA LIMIT, 2000uS MIN, 20000uS MAX
+//       UNO Progmem optimized to allow for small EXRAIL Automation scipts
+// 4.0.2 Command Station and EX-RAIL Ehancements & Additions:
+//       New JA, JR, JT commands availabe for Throttle Developers to obtain Route, Roster and Turnout descriptions for communications
+//       Change ACK defaults now set to LIMIT 50mA, MIN 2000uS, MAX 20000uS for more compatibility with non NMRA compliant decoders
+//       New Commands for the Arduino IDE Serial Monitor and JMRI DCC++ Traffic Monitor
+//        </RED signal_id>   to turn a individual LED Signal On & Off
+//        </AMBER signal_id> "
+//        </GREEN signal_id> "
+//        </KILL ALL> command to stop all tasks, and Diagnostic messages when KILL is used
+//        <t cab> command to obtain current throttle settings 
+//       Allow WRITE CV on PROG <W CV VALUE>
+//       Updated CV read command <R cv>. Equivalent to <V cv 0>.  Uses the verify callback.
+//       Change callback parameters are now optional on PROG
+//       Code: Fix weak reference to myFilter
 //       myFilter automatic detection (no need to call setFilter)
-//       FIX negative route ids in WIthrottle problem. 
-//       IFRED(signal_id), IFAMBER(signal_id), IFGREEN(signal_id)
-//       </RED signal_id> </AMBER signal_id> </GREEN signal_id> commands
-//       <t cab> command to obtain current throttle settings 
-//       JA, JR, JT commands to obtain route, roster and turnout descriptions
-//       HIDDEN turnouts
-//       PARSE <> commands in EXRAIL
-//       VIRTUAL_TURNOUT
-//       </KILL ALL> and KILLALL command to stop all tasks. 
-//       FORGET forgets the current loco in DCC reminder tables.
-//       Servo signals (SERVO_SIGNAL) 
-//       High-On signal pins (SIGNALH)
-//       Wait for analog value (ATGTE, ATLT)  
-// 4.0.1 Small EXRAIL updates
+//       Improved SIGNALs startup and diagnostics
+//       Incoming LCN turnout throw
+//       Allow turnout ID of "0"
+//       Code: struct TurnoutData to enable EEPROM in v 4.0 format
+//       Broadcast jopin after DriveAway
+//       Corrections to I2C code:
+//         1) I2CManager_Mega4809.h: Correct bitwise 'and' to logical 'and' - no impact.
+//         2) I2CManager_Wire.h: Ensure that error codes from Wire subsystem are passed back to caller in queueRequest().
+//       New EX-RAIL Functions to use in Automation(n), ROUTE(N) & SEQUENCE(N) scripts
+//        Automatically assign a name with ALIAS(name) without having to define it first
+//        ALIAS now has the aility to use Low underscore in keywords e.g., MY_KEYWORD
+//        ATGTE & ATLT wait for analog value, (At Greater Than or Equal and At Less Than a certain value)
+//        FADE command now works for LEDs connected on PCA9685 Servo/Signal board Output vpins 
+//        FORGET Forgets the current loco in DCC reminder tables saving memory and wasted packets sent to the track
+//        "IF" signal detection with IFRED(signal_id), IFAMBER(signal_id), IFGREEN(signal_id)
+//        KILLALL command to stop all tasks, and Diagnostic messages when KILL is used
+//        PARSE <> commands in EXRAIL allows sending of DCC-EX commands from EX-RAIL
+//        SERVO_SIGNAL Servo signals assigned to a specific servo turnout 
+//        SIGNALH High-On signal pins (Arduino normally handles active LOW signals. This allows for active HIGH)
+//        HIDDEN turnouts (hide a REAL turnout and create a VIRTUAL turnout to handle actions that happen BEFORE a turnout is thrown)
+//        VIRTUAL_TURNOUT definition
+//       README.md: removed misleading "folder/subforlders" (#218)
+//       README.md: fix dead link to rewrite (#217) in notes/rewrite.md
+// 4.0.1 Additional EXRAIL updates
 //       EXRAIL BROADCAST("msg") 
-//       EXRAIL POWERON
-// 4.0.0 Major functional and non-functional changes.
+//       EXRAIL POWERON (only turns on MAIN)
+//       Remove optional EXRAIL/ENDEXRAIL from myAutomation.example.h (#215)
+//       Use "startup sequence" to describe the initial instructions
+//       Add description of display scroll modes
+//       GetLocoCallback() restructured for better readability and put broadcastPower() at right place
+// 4.0.0 Major Production Release with New Functional and non-functional changes.
 //       Engine Driver "DriveAway" feature enhancement
 //       'Discovered Server' multicast Dynamic Network Server (mDNS) displays available WiFi connections to a DCC++EX Command Station
 //       New EX-RAIL "Extended Railroad Automation Instruction Language" automation capability.
