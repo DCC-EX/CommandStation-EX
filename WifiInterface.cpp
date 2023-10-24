88f1c0c580f3b94d935c7de6c714bd2f86acd66f
travis-farmer <travis.farmer@outlook.com>
2023-10-24 18:35:15 -0400

    needed for wifi on giga, added serial ports

--- WifiInterface.cpp
+++ WifiInterface.cpp
@@ -57,6 +57,14 @@ Stream * WifiInterface::wifiStream;
 #define SERIAL3 Serial3
 #endif
 
+#if (defined(ARDUINO_GIGA))
+#define NUM_SERIAL 5
+#define SERIAL1 Serial1
+#define SERIAL2 Serial2
+#define SERIAL3 Serial3
+#define SERIAL4 Serial4
+#endif
+
 #if defined(ARDUINO_ARCH_STM32)
 // Handle serial ports availability on STM32 for variants!
 // #undef NUM_SERIAL
