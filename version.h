818e05b4253a1a0980abb3a0bbef38a8c662bb1a
Harald Barth <haba@kth.se>
2024-01-10 08:37:54 +0100

    version 5.0.8

--- version.h
+++ version.h
@@ -3,7 +3,8 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.0.7"
+#define VERSION "5.0.8"
+// 5.0.8  - Bugfix: Do not crash on turnouts without description
 // 5.0.7  - Only flag 2.2.0.0-dev as broken, not 2.2.0.0
 // 5.0.6  - Bugfix lost TURNOUTL description
 // 5.0.5  - Bugfix version detection logic and better message
