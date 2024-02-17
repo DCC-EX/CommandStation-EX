fe9b1da8a3bcdced2465074dd882689d93e29b31
Harald Barth <haba@kth.se>
2024-02-17 18:52:48 +0100

    version 5.2.35

--- GITHUB_SHA.h
+++ GITHUB_SHA.h
@@ -1,1 +1,1 @@
-#define GITHUB_SHA "devel-202402050827Z"
+#define GITHUB_SHA "devel-202402171752Z"
--- version.h
+++ version.h
@@ -3,7 +3,8 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.2.34"
+#define VERSION "5.2.35"
+// 5.2.35 - Bugfix: Make DCC Extended Accessories follow RCN-213
 // 5.2.34 - <A address aspect> Command fopr DCC Extended Accessories
 //        - Exrail ASPECT(address,aspect) for above.
 //        - EXRAIL DCCX_SIGNAL(Address,redAspect,amberAspect,greenAspect)
