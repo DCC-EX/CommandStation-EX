d3038317738249e12063ef0b10f5045f485ce198
Harald Barth <haba@kth.se>
2024-03-11 14:52:55 +0100

    Functions for DC frequency: Use func up to F31

--- DCC.cpp
+++ DCC.cpp
@@ -242,7 +242,7 @@ void DCC::changeFn( int cab, int16_t functionNumber) {
 // Report function state (used from withrottle protocol)
 // returns 0 false, 1 true or -1 for do not know
 int8_t DCC::getFn( int cab, int16_t functionNumber) {
-  if (cab<=0 || functionNumber>28)
+  if (cab<=0 || functionNumber>31)
     return -1;  // unknown
   int reg = lookupSpeedTable(cab);
   if (reg<0)
