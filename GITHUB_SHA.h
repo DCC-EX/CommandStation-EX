7395aa4af826e86a28e1dd6cd4356b0762d59317
Harald Barth <haba@kth.se>
2024-08-29 13:46:44 +0200

    version

--- GITHUB_SHA.h
+++ GITHUB_SHA.h
@@ -1,1 +1,1 @@
-#define GITHUB_SHA "devel-202408080849Z"
+#define GITHUB_SHA "devel-202408291145Z"
--- version.h
+++ version.h
@@ -3,7 +3,8 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.2.74"
+#define VERSION "5.2.75"
+// 5.2.75 - Bugfix: Serial lines 4 to 6 OK
 // 5.2.74 - Bugfix: ESP32 turn on the joined prog (as main) again after a prog operation
 // 5.2.73 - Bugfix: STM32 further fixes to shadowPORT entries in TrackManager.cpp for PORTG and PORTH
 // 5.2.72 - Bugfix: added shadowPORT entries in TrackManager.cpp for PORTG and PORTH on STM32, fixed typo in MotorDriver.cpp
