377f10e1c5c3536fb2c5c1f80f6299ba8660289b
Asbelos <asbelos@btinternet.com>
2024-12-19 13:19:34 +0000

    5.2.92

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.2.91"
+#define VERSION "5.2.92"
 // 5.2.92 - Bugfix: FADE power off fix, EXRAIL power diagnostic fix.
 // 5.2.91 - Bugfix: Neopixel I2C overlap check
 // 5.2.90 - Bugfix: EXRAIL EXTT_TURNTABLE() now has description as optional in line with ocumentation (also fixed DCC_TURNTABLE) 
