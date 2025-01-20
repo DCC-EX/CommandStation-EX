865f75dda4edf819662888b919aa65ec05945c9f
Harald Barth <haba@kth.se>
2025-01-20 22:41:47 +0100

    version 5.4.2

--- version.h
+++ version.h
@@ -3,7 +3,8 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.4.1"
+#define VERSION "5.4.2"
+// 5.4.2  - Reversed turnout bugfix
 // 5.4.1  - ESP32 bugfix packet buffer race
 // 5.4.0  - New version on master
 // 5.2.96 - EXRAIL additions XFWD() and XREV()
