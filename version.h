d14aa46d51c546b8eb8d1d393c2be3ccbbd16eb4
Asbelos <asbelos@btinternet.com>
2025-01-21 10:45:56 +0000

    5.5.11

--- version.h
+++ version.h
@@ -3,7 +3,8 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.5.10"
+#define VERSION "5.5.11"
+// 5.5.11 - (5.4.2) accessory command reverse
 // 5.5.10 - CamParser fix
 // 5.5.9  - (5.4.3) fix changeFn for functions 29..31
 // 5.5.8  - EXSensorCam clean up to match other filters and
