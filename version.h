e174060bd7285b5706d2b286b1543c695c33c34e
Ash-4 <81280775+Ash-4@users.noreply.github.com>
2025-04-26 21:34:21 -0500

    CMRI files added to devel 5.2.22

--- version.h
+++ version.h
@@ -4,6 +4,7 @@
 #include "StringFormatter.h"
 
 #define VERSION "5.5.22"
+//        - CMRI files included
 // 5.5.22 - (5.4.9) Handle non-compliant decoders returning 255 for cv 20 and confusing <R> with bad consist addresses.
 //        - DCC 5mS gap to same loco DCC packet restriction
 //        - Catch up MASTER for ESP32 IDF version check
