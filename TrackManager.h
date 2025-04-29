dbf43d6bf32cc50e4f3c36735f7cf4576e1bf170
Ash-4 <81280775+Ash-4@users.noreply.github.com>
2025-04-29 17:32:51 -0500

    NucleoTimerSync for DC mode tracks

--- TrackManager.h
+++ TrackManager.h
@@ -1,6 +1,7 @@
 /*
  *  © 2022 Chris Harlow
  *  © 2022-2024 Harald Barth
+ *  © 2025 Herb Morton
  *  © 2023 Colin Murdoch
  *  © 2025 Herb Morton
  * 
@@ -70,6 +71,7 @@ class TrackManager {
     static void setTrackPower(TRACK_MODE trackmode, POWERMODE powermode);
     static void setMainPower(POWERMODE mode) {setTrackPower(TRACK_MODE_MAIN, mode);}
     static void setProgPower(POWERMODE mode) {setTrackPower(TRACK_MODE_PROG, mode);}
+    static void setTrackPowerF439ZI(byte t);
 
     static const int16_t MAX_TRACKS=8;
     static inline int8_t numTracks() { return lastTrack + 1; }
