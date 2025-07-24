940d7d70e28e93debf7fc9e61e8486b8a3df64a2
Asbelos <asbelos@btinternet.com>
2025-07-24 15:49:20 +0100

    Low ram sensor groups

---
+++ SensorGroup.cpp
@@ -0,0 +1,54 @@
+#include "SensorGroup.h"
+
+// called in loop to check sensors
+void SensorGroup::checkAll() {
+    #ifdef EXRAIL_ACTIVE
+    doExrailSensorGroup(GroupProcess::CHECK, & USB_SERIAL);
+    #endif
+}
+
+// called by command to get sensor list   
+void SensorGroup::printAll(Print * serial) {
+    #ifdef EXRAIL_ACTIVE
+    doExrailSensorGroup(GroupProcess::PRINT,serial);
+    #endif
+}
+
+void SensorGroup::pullupAll() {
+    #ifdef EXRAIL_ACTIVE
+    doExrailSensorGroup(GroupProcess::PULLUP, & USB_SERIAL);
+    #endif
+}
+
+// called by EXRAIL constructed doExrailSensorGroup for each group 
+void SensorGroup::doSensorGroup(VPIN firstVpin, int nPins, byte* statebits,
+  GroupProcess action, Print * serial) {
+  
+  // Loop through the pins in the group  
+  for (auto i=0;i<nPins;i++) {   
+    // locate position of state bit
+    byte stateByte=i/8;
+    byte stateMask=1<<(i%8);
+    VPIN vpin= firstVpin+i;
+    switch(action) {
+      case GroupProcess::PULLUP:
+          IODevice::configureInput(vpin, true);
+          __attribute__ ((fallthrough)); // to  check the current state 
+    
+      case GroupProcess::CHECK:
+         // check for state change
+         if ((bool)(statebits[stateByte]&stateMask) ==IODevice::read(vpin)) break; // no change  
+         // flip state bit
+         statebits[stateByte]^=stateMask;
+         if (action==GroupProcess::PULLUP) break; 
+         // fall through to print the changed value  
+        __attribute__ ((fallthrough));
+      
+      case GroupProcess::PRINT:
+        StringFormatter::send(serial, F("<%c %d>\n"), 
+         (statebits[stateByte]&stateMask)?'Q':'q', firstVpin+i);
+         break;
+    } 
+  }
+}
+    
--- Sensors.cpp
+++ Sensors.cpp
@@ -91,6 +91,9 @@ decide to ignore the <q ID> return and only react to <Q ID> triggers.
 ///////////////////////////////////////////////////////////////////////////////
 
 void Sensor::checkAll(){
+
+  SensorGroup::checkAll();
+  
   uint16_t sensorCount = 0;
 
 #ifdef USE_NOTIFY
@@ -181,12 +184,13 @@ void Sensor::inputChangeCallback(VPIN vpin, int state) {
 ///////////////////////////////////////////////////////////////////////////////
 
 void Sensor::printAll(Print *stream){
-
-  if (stream != NULL) {
-    for(Sensor * tt=firstSensor;tt!=NULL;tt=tt->nextSensor){
-      StringFormatter::send(stream, F("<%c %d>\n"), tt->active ? 'Q' : 'q', tt->data.snum);
-    }
-  } // loop over all sensors
+  
+  if (stream == NULL) return; // Nothing to do
+    
+  SensorGroup::printAll(stream);
+  for(Sensor * tt=firstSensor;tt!=NULL;tt=tt->nextSensor){
+    StringFormatter::send(stream, F("<%c %d>\n"), tt->active ? 'Q' : 'q', tt->data.snum);
+  }
 } // Sensor::printAll
 
 ///////////////////////////////////////////////////////////////////////////////
