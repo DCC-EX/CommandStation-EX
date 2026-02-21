a65e8fc0b438fc8880482bff2bf202d2ffb22af5
Asbelos <asbelos@btinternet.com>
2026-02-21 21:29:33 +0000

    5.5.63

--- version.h
+++ version.h
@@ -3,7 +3,8 @@
 
 #include "StringFormatter.h"
 
-#define VERSION "5.5.62"
+#define VERSION "5.5.63"
+// 5.5.63 - Bugfix: EXRAIL IFLOCO did not handle -1 (loco not found)
 // 5.5.62 - EXRAIL IFLOCO(list)
 //        - NEOPIXEL automatic WHITE(if available)  when red=green=blue
 // 5.5.61 - Bugfix: PCA9685 driver did not handle mux I2C addresses for writing registers
