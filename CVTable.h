be0a3946017eef52d721a3327a97ec5fd9b554b3
Rosscoetrain <iam@rosscoe.com>
2026-08-11 22:32:38 +1000

    Add licence block to CVTable files.

--- CVTable.h
+++ CVTable.h
@@ -1,3 +1,24 @@
+/*
+ *  © 2020-2026 Chris Harlow
+ *  © 2026 Ross Scanlon
+ *  All rights reserved.
+ *  
+ *  This file is part of DCC-EX API
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**********************************************************************
 
 The NVS Table class for DCC-EX Command Station Nodes
