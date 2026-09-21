76746ee9c20243aacc2db7bbf138debf94593cdd
Harald Barth <haba@kth.se>
2026-09-21 23:50:22 +0200

    AVR: Fix that OCR2B is not reset to 0 when analogWrite(9,0) is run

--- MotorDriver.cpp
+++ MotorDriver.cpp
@@ -461,6 +461,10 @@ void MotorDriver::throttleInrush(bool on) {
     DCCTimer::DCCEXanalogWriteFrequency(brakePin, 7); // 7 means max
   }
   analogWrite(brakePin,duty);
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  if (!on && brakePin == 9) // For RailCom cutout: need to reset the OCR2B mangled by analogWrite
+    OCR2B = 0;
+#endif
 #endif
 }
 unsigned int MotorDriver::raw2mA( int raw) {
